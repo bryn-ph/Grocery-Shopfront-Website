0cb930d7d165479d61d77bd5d9dbff97b27cca0a
Bryn Phillips <s3981189@student.rmit.edu.au>
2024-07-29 16:49:31 +1000

    deployment files

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // Add the react plugin automatically to every js file in the project
   plugins: [react()],
+  base: "/my-react-app/",
 })
